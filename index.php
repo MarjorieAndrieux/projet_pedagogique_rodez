d7ccd063565afd07f838e5f1bd94309ad99af111
thomas <onixyros@gmail.com>
2018-04-06 17:09:20 +0200

    mise en place owl_carousel

--- index.php
+++ index.php
@@ -1,3 +1,64 @@
+<?php
+$connect= mysqli_connect("localhost", "root", "Elpinus09", "pp_rodez");
+$connect->query("set names UTF8");
+
+// function requete($connect)
+// {
+//     $requete ="SELECT * FROM projet ORDER BY pro_date";
+//     $req_projets = mysqli_query($connect, $requete);
+//     return $req_projets;
+// }
+
+//Carousel 
+// function fleches_carousel($connect)
+// {
+// $res_carousel='';
+// $compteur=0;
+// $req_projets= make_query($connect);
+// while($ligne=mysqli_fetch_row($req_projets))
+// {
+//     if ($compteur == 0)
+//     {
+//         $res_carousel .= '
+//         <li data-target="#carousel_dynamique" data-slide-to="'.$compteur.'" class="active"></li>';
+//     }
+//     else
+//     {
+//         $res_carousel .= '
+//         <li data-target="#carousel_dynamique" data-slide-to="'.$compteur.'"></li> ';
+//     }
+//     $compteur = $compteur + 1;
+// }
+// return $res_carousel;
+// };
+
+// function carousel($connect)
+// {
+//     $res_carousel= '';
+//     $compteur = 0;
+//     $req_projets = make_query($connect);
+//     while ($ligne=mysqli_fetch_row($req_projets))
+//     {
+//         if($compteur==0)
+//         {
+//             $res_carousel .= '<div class="item-active">';
+//         }
+//         else
+//         {
+//             $res_carousel .= '<div class="item">';
+//         }
+//         $res_carousel .= '<img class="d-block w-100" src="'.$ligne[3].'" alt="slide 1 : correspond au projet le plus récent n°'.$compteur.'">
+//         <div class="carousel-caption">
+//             <h3>'.$ligne[1].'</h3>
+//         </div>
+//         </div>';
+//         $compteur = $compteur + 1;
+//     }
+// return $res_carousel;
+// }
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,42 +67,52 @@
     <title>Nom projet</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/jquery.fullpage.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
 </head>
 <body>
 <!-- Ne faites pas attention aux couleurs elles sont moches c'est juste histoire de travailler sur les div, nous n'avons pas encore trouvé de charte graphique -->
 <div class="container-fluid">
-    <div class="row cont_navbar">
-        <?php include("navbar.php"); ?>
-    </div>
-    <div class="row">
-        <div class="col-md-6 col-xs-12 projets" id="projets_alea">
-            Cette partie affichera les projet terminés aléatoirement (sans tags impliqués dans leur affichage)
-            <br>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio mollitia corrupti nemo quidem id odio earum. Mollitia a autem voluptatem rem cumque magnam id enim labore. Magni   dolore iste et!
+        <div class="row cont_navbar">
+            <?php include("navbar.php");?>
         </div>
-        <div class="col-md-6 col-xs-12 projets" id="projets_tags">
-            Cette partie affichera les projets terminés correspondant aux tags de l'utilisateur connecté
-            <br>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat excepturi nesciunt adquia     ipsa illum animi harum fuga reiciendis minima sequi eligendi ab corrupti eiusnecessitatibus    in vero, recusandae consequatur?
+        <div class="row">
+            <div class="col-md-6 col-xs-12 projets" id="projets_alea">
+                <!-- Cette partie affichera les projet terminés aléatoirement (sans tags impliqués dans leur    affichage) -->
+                <?php $req_projets=mysqli_query($connect, "SELECT * FROM projet ORDER BY pro_date LIMIT 10"); ?>
+                <div class="owl-carousel loop">
+                    <?php  while($ligne=mysqli_fetch_row($req_projets))
+                    {
+                        echo '
+                        <div> <img src="'.$ligne[3].'"> <h3>'.$ligne[1].'</h3> </div>
+                        ';                    
+                    }
+                    ?>
+                </div>
+            </div>
+            <div class="col-md-6 col-xs-12 projets" id="projets_tags">
+                <!-- Cette partie affichera les projets terminés correspondant aux tags de l'utilisateur connecté -->
+            </div>
         </div>
-    </div>
 
-    <div class="row" id="cont_aide_projets">
-        Cette partie affichera les projets ayant besoin de contributeurs
-        <br>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, repellat reiciendis? Sed id quod    reprehenderit cupiditate reiciendis error, repudiandae quis exercitationem, ipsam odio consectetur     velit illo eos amet totam doloribus!
-    </div>
+        <div class="row" id="cont_aide_projets">
+            Cette partie affichera les projets ayant besoin de contributeurs
+            <br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, repellat reiciendis? Sed id quod     reprehenderit cupiditate reiciendis error, repudiandae quis exercitationem, ipsam odio  consectetur     velit illo eos amet totam doloribus!
+        </div>
 
-    <div class="row" id="cont_footer">
-        <?php include("footer.html")?>
-    </div>
-
+        <div class="row" id="cont_footer">
+            <?php include("footer.html")?>
+        </div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="js/jquery.fullpage.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
 <script src="js/main.js"></script>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-<<<<<<< HEAD
-//Fichier peut être obsolète pour l'instant il ne servait que pour l'inclusion de la navbar avec la fonction load, que l'on a remplacé par un include en php pour plus de fiabilité (l'utilisateur peut désactivier le js sur son navigateur ce qui est problématique)
-
-// $(function(){
-//     $(".cont_navbar").load("navbar.php");
-//   });
-=======
-$(function(){
-    $(".cont_navbar").load("navbar.html");
-  });
-
->>>>>>> d7d4a246c9d9cd7e8786f36c398139c9e593dc4e
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(
+    {
+        center: true,
+        items:3,
+        loop:true,
+        center:true,
+        nav:true,
+        rewind:false,
+        lazyLoad:true,
+        autoplay:true,
+        autoplayHoverPause:true,
+        responsive:{
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    })
+});
