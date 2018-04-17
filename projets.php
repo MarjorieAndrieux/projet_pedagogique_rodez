f540b92a9d35e06a7fe11ff3a47dd9ebf96139f2
Marjorie Andrieux <andrieux.marjoriebeatricelydie@gmail.com>
2018-04-18 00:11:33 +0200

    mise a jour titre

--- projets.php
+++ projets.php
@@ -9,7 +9,7 @@ $connect->query("set names UTF8");
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Projets</title>
+    <title>Share it!</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
