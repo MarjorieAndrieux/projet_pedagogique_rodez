9c04a01ce9404117e2f19c6835a5757180c29d95
thomas <onixyros@gmail.com>
2018-04-18 09:57:43 +0200

    commit changement branche

--- requete_connexion_navbar.php
+++ requete_connexion_navbar.php
@@ -5,13 +5,12 @@ $connect->query("set names UTF8");
 $mail=$_POST['mail'];
 $mdp=$_POST['mdp'];
 
-var_dump($mail);
-var_dump($mdp);
-
 $requete_cookie = mysqli_query($connect, "SELECT util_id,util_email,util_mdp,util_pseudo FROM utilisateur WHERE util_email='".$mail."'");
 $result_cookie = mysqli_fetch_array($requete_cookie);
+$mailconnexion = $result_cookie[1];
+$mdpconnexion = $result_cookie[2];
+$pseudoconnexion = $result_cookie[3];
 
-var_dump($result_cookie);
 
 if(isset($_POST['mail']) && isset($_POST['mdp']) )
 {   
@@ -21,13 +20,25 @@ if(isset($_POST['mail']) && isset($_POST['mdp']) )
         session_start();
         //Définition cookie : d'abord on a le nom du cookie (ici cookie_mail) ensuite on appelle la variable qui va lui donner sa valeur, ensuite on définit sa durée de vie (ici 1 mois) et les arguments finaux servent à ajouter un peu de sécurité (le null null false je ne sais pas ce qu'ils font mais le true sert à activer le mode httpOnly)
         setcookie('cookie_mail',$mail_cookie,time()+31*24*3600,null,null,false,true);
-        echo $mailconnexion;
-        
+        //Popup permettant d'indiquer a l'utilisateur qu'il est bien connecté
+        $message="Vous êtes désormais connecté en tant que :".$pseudoconnexion." ";
+        echo '<script type="text/javascript">window.alert("'.$message.'");</script>';
     }
+    else if($mail == $mailconnexion && $mdp != $mdpconnexion)
+    {
+        $message="Votre mot de passe n'est pas bon ";
+        echo '<script type="text/javascript">window.alert("'.$message.'");</script>';
+    }
+    //Condition a retravailler : si le mail n'est pas le bon la requête ne peux pas retrouver le mot de passe qui lui est associé
+    // else if($mail != $mailconnexion && $mdp == $mdpconnexion)
+    // {
+    //     $message="Votre mail n'est pas bon ";
+    //     echo '<script type="text/javascript">window.alert("'.$message.'");</script>';
+    // }
     else
     {
-        echo "Problème de connexion";
-        // var_dump($result_cookie);
+        $message="Vos identifiants ne sont pas bon ";
+        echo '<script type="text/javascript">window.alert("'.$message.'");</script>';
     }
 }
 ?>
